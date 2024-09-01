41750d53384f0aa01c62668e6feca6fb7667c09e
AlaguPrasanna <alaguprasanna1812@gmail.com>
2024-09-01 16:01:24 +0530

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet,TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
